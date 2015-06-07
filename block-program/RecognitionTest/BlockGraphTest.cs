ac297adf013c9be3adae67911a857c6e706fde91
elda27 <kaz.birdstick@gmail.com>
2015-06-08 00:22:09 +0900

    [add]:Testプロジェクト
    とりあえず追加しておきます

---
+++ block-program/RecognitionTest/BlockGraphTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Myxini.Recognition;
+
+namespace RecognitionTest
+{
+	[TestClass]
+	public class BlockGraphTest
+	{
+		private BlockGraph Graph = new BlockGraph();
+
+		[TestMethod]
+		public void AddTest()
+		{
+		}
+
+		[TestMethod]
+		public void GetTest()
+		{
+		}
+
+		[TestMethod]
+		public void PreviousAccessTest()
+		{
+		}
+	}
+}
