539846e8c2a9bc44b48d0f9ca0dd40f745d6d305
elda27 <kaz.birdstick@gmail.com>
2015-06-08 00:59:39 +0900

    [add]:センサー周りのブロックのオブジェクトを追加しました

---
+++ block-program/Detection/MicroSwitchBlock.cs
@@ -0,0 +1,10 @@
+﻿namespace Myxini.Recognition
+{
+	public class MicroSwitchBlock : SensorBlock
+	{
+		MicroSwitchBlock(bool state) : base(Command.MicroSwitch, new BlockParameter(new int[]{state ? 1 : 0}))
+		{
+
+		}
+	}
+}
--- block-program/Detection/PSDSensorBlock.cs
+++ block-program/Detection/PSDSensorBlock.cs
@@ -3,7 +3,7 @@ namespace Myxini.Recognition
 {
 	public class PSDSensorBlock : SensorBlock
 	{
-		public PSDSensorBlock(int distance) : base(Command.PSD, new BlockParameter())
+		public PSDSensorBlock(int distance) : base(Command.PSD, new BlockParameter(new int[]{distance, 0}))
 		{
 
 		}
