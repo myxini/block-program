4ece1f7ef2718a0cb58dcf2f83105329e366d720
xi-xi <qhn.068@gmail.com>
2015-06-10 10:55:55 +0900

    [add]:Pc2RobotPacket
    PCからロボットへのパケットを表すクラスを追加しました

---
+++ block-program/Communication/Robot/Pc2RobotPacket.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myxini.Communication.Robot
+{
+    class Pc2RobotPacket : Packet
+    {
+        protected const byte[] HEAD = {0x06, 0x09};
+    }
+}
