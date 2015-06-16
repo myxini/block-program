323a1e54c41e7e2df80885049405cb3b10258722
xi-xi <qhn.0618@gmail.com>
2015-06-17 00:55:07 +0900

    [add]: チェックサム計算メソッドの追加
    チェックサムを計算してくれる関数の作成

--- block-program/Communication/Robot/Packet.cs
+++ block-program/Communication/Robot/Packet.cs
@@ -6,5 +6,23 @@ namespace Myxini.Communication.Robot
     public abstract class Packet
     {
         protected abstract byte[] _packetData { get; }
+
+        private static byte calcCheckSum(byte[] packet)
+        {
+            byte sum = 0;
+            foreach(var b in packet)
+            {
+                sum ^= b;
+            }
+            return sum;
+        }
+
+        /// <summary>
+        /// PacketDataの最後の要素をチェックサムで埋めます
+        /// </summary>
+        protected void addCheckSum()
+        {
+            this._packetData[_packetData.Length - 1] = Packet.calcCheckSum(this._packetData);
+        }
     }
 }
