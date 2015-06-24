0969d7d0ae58bb4ce0ace4e161b0d8a2b4d9cf49
xi-xi <qhn.0618@gmail.com>
2015-06-24 11:11:31 +0900

    [fix]: 案の定マージしたら変なことになってたので修正

--- block-program/CommunicationTest/PacketTest.cs
+++ block-program/CommunicationTest/PacketTest.cs
@@ -55,11 +55,6 @@ namespace CommunicationTest
             }
             Assert.IsTrue(service.IsRunning);
             service.Stop();
-=======
-            var service = new CommunicationService("4");
-            var script = new Script();
-            service.Run(script);
->>>>>>> a91b32cff28944175dc22c95c07d19c01b3f1682
         }
     }
 }
