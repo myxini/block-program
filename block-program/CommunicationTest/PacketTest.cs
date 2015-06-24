3c38ca3f9330f8fb33f9b9ba7051807e9b491020
xi-xi <qhn.0618@gmail.com>
2015-06-24 10:42:21 +0900

    [add]: ポート開くテスト書きました

--- block-program/CommunicationTest/PacketTest.cs
+++ block-program/CommunicationTest/PacketTest.cs
@@ -8,12 +8,54 @@ namespace CommunicationTest
     [TestClass]
     public class PacketTest
     {
+        private TestContext testContextInstance;
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
         [TestMethod]
         public void RotateCommandTest()
         {
-            var service = new CommunicationService();
+            //var service = new CommunicationService();
             var script = new Script();
-            service.Run(script);
+            //service.Run(script);
+        }
+
+        [TestMethod]
+        public void SerialOpenTest()
+        {
+            var ports = CommunicationService.GetAvailablePorts();
+            CommunicationService service = null;
+            if (ports.Length == 1)
+            {
+                service = new CommunicationService(ports[0]);
+                service.Run(new Script());
+            }
+            else
+            {
+                foreach (var port in ports)
+                {
+                    service = new CommunicationService(port);
+                    service.Run(new Script());
+                    if (service.IsRunning)
+                    {
+                        break;
+                    }
+                }
+            }
+            Assert.IsTrue(service.IsRunning);
         }
     }
 }
