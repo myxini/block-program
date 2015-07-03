d2455ab4c6b6c69c7e2ab38f42ed3586e75575d4
elda27 <kaz.birdstick@gmail.com>
2015-07-04 00:37:18 +0900

    [add] 画像処理の追加
    DilationとErosionを追加しましtあ

--- block-program/Detection/Image/Process.cs
+++ block-program/Detection/Image/Process.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace Myxini.Recognition.Image
 {
-	public static class Process
+	public static class Process<Image>
+		where Image : IImage
 	{
-		int Dilate(IImage image, int x, int y, int c)
+		/// <summary>
+		/// 膨張処理
+		/// 基本的にコンストラクタにつっこんでください
+		/// </summary>
+		public static int Dilate(Image image, int x, int y, int c)
 		{
+			if(x < 0 || y < 0 || x >= image.Width || y >= image.Height)
+			{
+				return DilateSlow(image, x, y, c);
+			}
+
 			return
 				image.GetElement(x - 1, y - 1, c) != 0 ||
 				image.GetElement(x, y - 1, c) != 0 ||
@@ -20,6 +30,74 @@ namespace Myxini.Recognition.Image
 				image.GetElement(x - 1, y + 1, c) != 0 ||
 				image.GetElement(x, y + 1, c) != 0 ||
 				image.GetElement(x + 1, y + 1, c) != 0 ? int.MaxValue : 0;
+		}
+
+		/// <summary>
+		/// 膨張処理
+		/// 基本的にコンストラクタにつっこんでください
+		/// </summary>
+		public static int Erode(Image image, int x, int y, int c)
+		{
+			if (x < 0 || y < 0 || x >= image.Width || y >= image.Height)
+			{
+				return DilateSlow(image, x, y, c);
+			}
+
+			return
+				image.GetElement(x - 1, y - 1, c) == 0 ||
+				image.GetElement(x, y - 1, c) == 0 ||
+				image.GetElement(x + 1, y - 1, c) == 0 ||
+				image.GetElement(x - 1, y, c) == 0 ||
+				image.GetElement(x, y, c) == 0 ||
+				image.GetElement(x + 1, y, c) == 0 ||
+				image.GetElement(x - 1, y + 1, c) == 0 ||
+				image.GetElement(x, y + 1, c) == 0 ||
+				image.GetElement(x + 1, y + 1, c) == 0 ? 0 : int.MaxValue;
+		}
+
+		private static int DilateSlow(Image image, int x, int y, int c)
+		{
+			for(int py = y - 1; py < (y + 1); ++py)
+			{
+				for (int px = x - 1; px < (x + 1); ++px)
+				{
+					try
+					{
+						if (image.GetElement(px, py, c) != 0)
+						{
+							return int.MaxValue;
+						}
+					}
+					catch(Exception )
+					{
+
+					}
+				}
+			}
+
+			return 0;
+		}
+		private static int ErodeSlow(Image image, int x, int y, int c)
+		{
+			for (int py = y - 1; py < (y + 1); ++py)
+			{
+				for (int px = x - 1; px < (x + 1); ++px)
+				{
+					try
+					{
+						if (image.GetElement(px, py, c) == 0)
+						{
+							return 0;
+						}
+					}
+					catch (Exception)
+					{
+
+					}
+				}
+			}
+
+			return int.MaxValue;
 		}
 	}
 }
