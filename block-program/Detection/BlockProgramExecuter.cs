3ba2358d06d096bfbcb766d0293e45ed151760a3
Kazuaki HARA <asuka.kazuaki@gmail.com>
2015-07-08 08:53:46 +0900

    [add]: Add an executer for executing a block program

---
+++ block-program/Detection/BlockProgramExecuter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myxini.Recognition
+{
+    class BlockProgramExecuter : IBlockProgramExecuter
+    {
+        /// <summary>
+        /// ホワイトボード上に構成されたプログラムを1回読んで実行する
+        /// </summary>
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
