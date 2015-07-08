0973a5d5562052f619a39f1f9c959ebaaea8725c
elda27 <kaz.birdstick@gmail.com>
2015-07-08 11:36:37 +0900

    [Remove] 未使用のファイルを削除しました#4 IDetector

--- block-program/Detection/IDetector.cs
+++
@@ -1,9 +0,0 @@
-﻿using Myxini.Recognition.Image;
-
-namespace Myxini.Recognition
-{
-	interface IDetector
-	{
-//		bool Detection(IImage image, out Raw.BlockArray blocks, out Raw.LineArray lines);
-	}
-}
