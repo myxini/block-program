fcf7410dadcf6c60b5cd8032d9177cdc9837853e
elda27 <kaz.birdstick@gmail.com>
2015-07-10 20:58:10 +0900

    [Add] テストメソッドの追加（Raw.Sizeのコンストラクタ）

---
+++ block-program/RecognitionTest/RawTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Myxini.Recognition.Raw;
+
+namespace RecognitionTest
+{
+	[TestClass]
+	public class RawTest
+	{
+		[TestClass]
+		public class TestPoint
+		{
+			[TestMethod]
+			public void TestConstructor()
+			{
+				const int point_x = 20, point_y = 10;
+
+				var point = new Point(point_x, point_y);
+
+				Assert.Equals(point.X, point_x);
+				Assert.Equals(point.Y, point_y);
+			}
+		}
+
+	[TestClass]
+		public class TestSize
+		{
+			[TestMethod]
+			public void TestConstructor()
+			{
+				const int width = 20, height = 10;
+
+				var size = new Size(width, height);
+
+				Assert.Equals(size.Width, width);
+				Assert.Equals(size.Height, height);
+			}
+		}
+	}
+}
