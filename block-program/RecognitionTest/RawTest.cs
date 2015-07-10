e6fcf21321081ec65ec53e6189e86b1d5dbb9bb3
elda27 <kaz.birdstick@gmail.com>
2015-07-11 08:49:53 +0900

    [fix] テストをクラスからメソッドに変更しました

--- block-program/RecognitionTest/RawTest.cs
+++ block-program/RecognitionTest/RawTest.cs
@@ -12,34 +12,29 @@ namespace RecognitionTest
 	[TestClass]
 	public class RawTest
 	{
-		[TestClass]
-		public class TestPoint
+		[TestMethod]
+		public void TestPoint()
 		{
-			[TestMethod]
-			public void TestConstructor()
-			{
-				const int point_x = 20, point_y = 10;
+			const int point_x = 20, point_y = 10;
 
-				var point = new Point(point_x, point_y);
+			var point = new Point(point_x, point_y);
 
-				Assert.Equals(point.X, point_x);
-				Assert.Equals(point.Y, point_y);
-			}
+			Assert.Equals(point.X, point_x);
+			Assert.Equals(point.Y, point_y);
 		}
 
-	[TestClass]
-		public class TestSize
+		[TestMethod]
+		public void TestSize()
 		{
-			[TestMethod]
-			public void TestConstructor()
-			{
-				const int width = 20, height = 10;
+			const int width = 20, height = 10;
 
-				var size = new Size(width, height);
+			var size = new Size(width, height);
 
-				Assert.Equals(size.Width, width);
-				Assert.Equals(size.Height, height);
-			}
+			Assert.Equals(size.Width, width);
+			Assert.Equals(size.Height, height);
+
+			Assert.Equals(size.Area, width * height);
 		}
+
 	}
 }
