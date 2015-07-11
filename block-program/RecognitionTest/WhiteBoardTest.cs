12bc1bbd1a1b383683d3b484e6fc2a28306e4ffa
elda27 <kaz.birdstick@gmail.com>
2015-07-11 12:05:15 +0900

    [Add] デバッグ用のカメラクラスを追加しました

---
+++ block-program/RecognitionTest/WhiteBoardTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Myxini.Recognition;
+
+namespace RecognitionTest
+{
+	/// <summary>
+	/// WhiteBoardTest の概要の説明
+	/// </summary>
+	[TestClass]
+	public class WhiteBoardTest
+	{
+		public WhiteBoardTest()
+		{
+			//
+			// TODO: コンストラクター ロジックをここに追加します
+			//
+		}
+
+		private TestContext testContextInstance;
+
+		/// <summary>
+		///現在のテストの実行についての情報および機能を
+		///提供するテスト コンテキストを取得または設定します。
+		///</summary>
+		public TestContext TestContext
+		{
+			get
+			{
+				return testContextInstance;
+			}
+			set
+			{
+				testContextInstance = value;
+			}
+		}
+
+		#region 追加のテスト属性
+		//
+		// テストを作成する際には、次の追加属性を使用できます:
+		//
+		// クラス内で最初のテストを実行する前に、ClassInitialize を使用してコードを実行してください
+		// [ClassInitialize()]
+		// public static void MyClassInitialize(TestContext testContext) { }
+		//
+		// クラス内のテストをすべて実行したら、ClassCleanup を使用してコードを実行してください
+		// [ClassCleanup()]
+		// public static void MyClassCleanup() { }
+		//
+		// 各テストを実行する前に、TestInitialize を使用してコードを実行してください
+		// [TestInitialize()]
+		// public void MyTestInitialize() { }
+		//
+		// 各テストを実行した後に、TestCleanup を使用してコードを実行してください
+		// [TestCleanup()]
+		// public void MyTestCleanup() { }
+		//
+		#endregion
+
+		[TestMethod]
+		public void CalibrationTest()
+		{
+			var camera = new VirtualCamera();
+			camera.AddFrame(Myxini.Recognition.Image.)
+
+			var white_board = new WhiteBoard();
+		}
+	}
+}
