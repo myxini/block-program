61f1fe2b0b77fcdf8e0c3559303a9bfb6d6f3564
elda27 <kaz.birdstick@gmail.com>
2015-07-11 12:36:17 +0900

    [Add] ホワイトボードクラスのテストコードを書きましtあ

--- block-program/RecognitionTest/WhiteBoardTest.cs
+++ block-program/RecognitionTest/WhiteBoardTest.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Myxini.Recognition;
+using Myxini.Recognition.Image;
 
 namespace RecognitionTest
 {
@@ -60,13 +61,64 @@ namespace RecognitionTest
 		//
 		#endregion
 
+		IImage LoadImage(string filename)
+		{
+			var bitmap = new System.Drawing.Bitmap(filename);
+
+			var pixels = new byte[bitmap.Width * bitmap.Height * 3];
+			for(int y = 0; y < bitmap.Height; ++y)
+			{
+				for(int  x = 0; x < bitmap.Width; ++x)
+				{
+					var color = bitmap.GetPixel(x, y);
+					pixels[y * bitmap.Width + x + 0] = color.B;
+					pixels[y * bitmap.Width + x + 1] = color.G;
+					pixels[y * bitmap.Width + x + 2] = color.R;
+				}
+			}
+
+			var image = new ColorImage(pixels, bitmap.Width, bitmap.Height);
+			return image;
+		}
+
 		[TestMethod]
 		public void CalibrationTest()
 		{
 			var camera = new VirtualCamera();
-//			camera.AddFrame(Myxini.Recognition.Image.)
+
+			// ホワイトボードの画像が5枚必要
+			camera.AddFrame(LoadImage("Test_Whiteboad_Frame1.bmp"));
+			camera.AddFrame(LoadImage("Test_Whiteboad_Frame2.bmp"));
+			camera.AddFrame(LoadImage("Test_Whiteboad_Frame3.bmp"));
+			camera.AddFrame(LoadImage("Test_Whiteboad_Frame4.bmp"));
+
+			var image = LoadImage("Test_Whiteboad_Frame5.bmp");
 
 			var white_board = new WhiteBoard();
+			white_board.Calibration(camera);
+			var background_deleted_image = white_board.GetBackgroundDeleteImage(image);
+
+			int white_count = 0;
+			for(int y = 0; y < background_deleted_image.Height; ++y)
+			{
+				for(int x = 0;x < background_deleted_image.Width; ++x)
+				{
+					var r = background_deleted_image.GetElement(x, y, 0);
+					var g = background_deleted_image.GetElement(x, y, 1);
+					var b = background_deleted_image.GetElement(x, y, 2);
+
+					// ホワイトボード平面だけを切りだすので画像は白くなるはず
+					if((r - g) * (r -b) < 49)
+					{
+						++white_count;
+					}
+				}
+			}
+
+
+			Assert.IsTrue(white_count < image.Width * image.Height * 0.1);
 		}
+
+
 	}
 }
