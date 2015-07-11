155874bbac5ec4549fd9f7ec92c4db77f222d073
elda27 <kaz.birdstick@gmail.com>
2015-07-11 13:23:53 +0900

    [Add] Clussifierのテスト書いて途中ですがコミットします

--- block-program/RecognitionTest/WhiteBoardTest.cs
+++ block-program/RecognitionTest/WhiteBoardTest.cs
@@ -60,39 +60,19 @@ namespace RecognitionTest
 		// public void MyTestCleanup() { }
 		//
 		#endregion
-
-		IImage LoadImage(string filename)
-		{
-			var bitmap = new System.Drawing.Bitmap(filename);
-
-			var pixels = new byte[bitmap.Width * bitmap.Height * 3];
-			for(int y = 0; y < bitmap.Height; ++y)
-			{
-				for(int  x = 0; x < bitmap.Width; ++x)
-				{
-					var color = bitmap.GetPixel(x, y);
-					pixels[y * bitmap.Width + x + 0] = color.B;
-					pixels[y * bitmap.Width + x + 1] = color.G;
-					pixels[y * bitmap.Width + x + 2] = color.R;
-				}
-			}
-
-			var image = new ColorImage(pixels, bitmap.Width, bitmap.Height);
-			return image;
-		}
-
+		
 		[TestMethod]
 		public void CalibrationTest()
 		{
 			var camera = new VirtualCamera();
 
 			// ホワイトボードの画像が5枚必要
-			camera.AddFrame(LoadImage("Test_Whiteboad_Frame1.bmp"));
-			camera.AddFrame(LoadImage("Test_Whiteboad_Frame2.bmp"));
-			camera.AddFrame(LoadImage("Test_Whiteboad_Frame3.bmp"));
-			camera.AddFrame(LoadImage("Test_Whiteboad_Frame4.bmp"));
+			camera.AddFrame(ImageLoader.LoadImage("Test_Whiteboad_Frame1.bmp"));
+			camera.AddFrame(ImageLoader.LoadImage("Test_Whiteboad_Frame2.bmp"));
+			camera.AddFrame(ImageLoader.LoadImage("Test_Whiteboad_Frame3.bmp"));
+			camera.AddFrame(ImageLoader.LoadImage("Test_Whiteboad_Frame4.bmp"));
 
-			var image = LoadImage("Test_Whiteboad_Frame5.bmp");
+			var image = ImageLoader.LoadImage("Test_Whiteboad_Frame5.bmp");
 
 			var white_board = new WhiteBoard();
 			white_board.Calibration(camera);
