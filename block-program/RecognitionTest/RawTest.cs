2dc07876359eb7ddf28e184145b0afb7ecadb5c3
elda27 <kaz.birdstick@gmail.com>
2015-07-11 10:11:43 +0900

    [Add] Recognition.Rawのテストを書きました

--- block-program/RecognitionTest/RawTest.cs
+++ block-program/RecognitionTest/RawTest.cs
@@ -1,17 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Myxini.Recognition.Raw;
+
 
 namespace RecognitionTest
 {
 	[TestClass]
 	public class RawTest
 	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+
 		[TestMethod]
 		public void TestPoint()
 		{
@@ -36,5 +38,25 @@ namespace RecognitionTest
 			Assert.Equals(size.Area, width * height);
 		}
 
+		[TestMethod]
+		public void TestRectangle()
+		{
+			const int left = 20, top = 10, width = 300, height = 200;
+
+			var rect = new Rectangle(new Point(left, top), new Size(width, height));
+
+			var rect2 = new Rectangle(new Point(left, top), new Point(left + width, top + height));
+
+			Assert.Equals(rect.Position.X, rect2.Position.X);
+			Assert.Equals(rect.Position.Y, rect2.Position.Y);
+			Assert.Equals(rect.BoundingSize.Width, rect2.BoundingSize.Width);
+			Assert.Equals(rect.BoundingSize.Height, rect2.BoundingSize.Height);
+
+			var rect3 = new Rectangle(left, top, width, height);
+			Assert.Equals(rect.Position.X, rect3.Position.X);
+			Assert.Equals(rect.Position.Y, rect3.Position.Y);
+			Assert.Equals(rect.BoundingSize.Width, rect3.BoundingSize.Width);
+			Assert.Equals(rect.BoundingSize.Height, rect3.BoundingSize.Height);
+		}
 	}
 }
