fa6bdbd6602bc90875c2f9a412088d07db32e58a
Kazuaki HARA <asuka.kazuaki@gmail.com>
2015-07-12 14:48:39 +0900

    [add]: add Algorithm interface and class

---
+++ block-program/Detection/IPatternMatchingAlgorithm.cs
@@ -0,0 +1,28 @@
+﻿using Myxini.Recognition.Image;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myxini.Recognition
+{
+    public interface IPatternMatchingAlgorithm
+    {
+        /// <summary>
+        /// raw_blockと一番よくマッチするpatternを返す
+        /// </summary>
+        /// <param name="patterns"></param>
+        /// <param name="raw_block"></param>
+        /// <returns></returns>
+        Pattern Match(IEnumerable<Pattern> patterns, IImage raw_block);
+
+        /// <summary>
+        /// imageとpatternのマッチする具合を返す
+        /// </summary>
+        /// <param name="pattern"></param>
+        /// <param name="image"></param>
+        /// <returns></returns>
+        double Match(IImage pattern, IImage image);
+    }
+}
