72cdc0273930ced0fcb090053f18ff8ae0b9b662
Kazuaki HARA <asuka.kazuaki@gmail.com>
2015-07-15 10:25:46 +0900

    [add]: add basic UI for selecting lectangle

--- block-program/UI/MainWindow.xaml.cs
+++ block-program/UI/MainWindow.xaml.cs
@@ -24,5 +24,51 @@ namespace UI
         {
             InitializeComponent();
         }
+
+        private Point position_down;
+
+        private bool is_down = false;
+
+        public System.Drawing.Rectangle Area { get; private set; }
+
+        private void ContentPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point position = e.GetPosition(ContentPanel);
+            position_down = position;
+            is_down = true;
+            Rectangle1.Margin = new Thickness(position.X, position.Y, 0, 0);
+            Rectangle1.Width = 0;
+            Rectangle1.Height = 0;
+            Rectangle1.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void ContentPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!is_down)
+            {
+                return;
+            }
+            Point position = e.GetPosition(ContentPanel);
+            Rectangle1.Width = Math.Abs(position.X - position_down.X);
+            Rectangle1.Height = Math.Abs(position.Y - position_down.Y);
+            Rectangle1.Margin = new Thickness(
+                Math.Min(position.X, position_down.X),
+                Math.Min(position.Y, position_down.Y),
+                0,
+                0
+            );
+        }
+
+        private void ContentPanel_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            is_down = false;
+            Area = new System.Drawing.Rectangle(
+                (int) Rectangle1.Margin.Left,
+                (int) Rectangle1.Margin.Top,
+                (int) Rectangle1.Width,
+                (int) Rectangle1.Height
+            );
+
+        }
     }
 }
