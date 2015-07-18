65e6209567eed644eed781511d89d8718e87771b
xi-xi <qhn.0618@gmail.com>
2015-07-18 16:49:35 +0900

    [modify]: PSDのしきい値変更
    PSDの閾値を設定しなおし、そこそこまともな値で変更するようにしました

--- block-program/Communication/CommunicationService.cs
+++ block-program/Communication/CommunicationService.cs
@@ -35,7 +35,7 @@ namespace Myxini.Communication
         const byte SENSORID_MICROSWITCH = 0x02;
         #endregion
         #region ロボットにおいてどのスレッドが走るかを決めるしきい値群
-        private const int STATE_PSD_THRES = 60;
+        private const int STATE_PSD_THRES = 300;
         private const int STATE_MICROSWITCH_THRES = 1;
         #endregion
         private Dictionary<Command, Robot.CommandList> _robotScript;
@@ -113,7 +113,8 @@ namespace Myxini.Communication
 
         private void UpdateCurrentCommand(byte sensorID, ushort sensorValue)
         {
-            if(sensorID == SENSORID_PSD && sensorValue < STATE_PSD_THRES)
+            System.Diagnostics.Debug.WriteLine(sensorID.ToString() + ": " + sensorValue.ToString());
+            if(sensorID == SENSORID_PSD && sensorValue >= STATE_PSD_THRES)
             {
                 this._currentInstructionType = Command.PSD;
             }
